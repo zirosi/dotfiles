eb77ff0b920c579543647ba9b404933afe8e03e8
zirosi <kaidion@proton.me>
2025-05-27 14:48:09 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,27 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4N
+
+-- Add to your config.lua
+local M = {}
+
+M.ui = {
+  -- Enable icons globally
+  icons = {
+    enabled = true,
+  },
+}
+
+local function open_nvim_tree()
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+vim.opt.clipboard = "unnamedplus"
+
+lvim.keys.normal_mode["<C-t>"] = ":bot term<CR>"
+
+return M
